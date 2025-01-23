e6ac4613e570c098cc95155c7b7e12d4edb240f8
ufakish <phalishort@gmail.com>
2025-01-23 09:18:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ufakish/wyz",
+    "https://github.com/ufakish/jmz",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
